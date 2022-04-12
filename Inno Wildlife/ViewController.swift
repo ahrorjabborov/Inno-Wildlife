2d8ad864080d223415897fd7c7cfa4c1f7caeae7
Ahror Jabborov <ahror7j@gmail.com>
2022-04-12 19:17:33 +0300

    Initial Commit

---
+++ Inno Wildlife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inno Wildlife
+//
+//  Created by Ahror Jabborov on 4/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
